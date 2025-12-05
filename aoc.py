8d998621ef3b2f83ba620428aca2b5214e608d7a
NJKode <nathan.kennedy.1997@gmail.com>
2025-12-06 11:15:03 +1300

    Remaining boilerplate

--- aoc.py
+++ aoc.py
@@ -6,6 +6,13 @@ import day02.python.solution as day02
 import day03.python.solution as day03
 import day04.python.solution as day04
 import day05.python.solution as day05
+import day06.python.solution as day06
+import day07.python.solution as day07
+import day08.python.solution as day08
+import day09.python.solution as day09
+import day10.python.solution as day10
+import day11.python.solution as day11
+import day12.python.solution as day12
 
 
 solutions = {
@@ -19,14 +26,27 @@ solutions = {
     (4, 2): day04.part_two,
     (5, 1): day05.part_one,
     (5, 2): day05.part_two,
+    (6, 1): day06.part_one,
+    (6, 2): day06.part_two,
+    (7, 1): day07.part_one,
+    (7, 2): day07.part_two,
+    (8, 1): day08.part_one,
+    (8, 2): day08.part_two,
+    (9, 1): day09.part_one,
+    (9, 2): day09.part_two,
+    (10, 1): day10.part_one,
+    (10, 2): day10.part_two,
+    (11, 1): day11.part_one,
+    (11, 2): day11.part_two,
+    (12, 1): day12.part_one,
+    (12, 2): day12.part_two,
 }
 
 
 @click.command()
-@click.argument('day')
-@click.argument('part')
-@click.option('-t', '--test', is_flag=True,
-              help='Run the test.txt file')
+@click.argument("day")
+@click.argument("part")
+@click.option("-t", "--test", is_flag=True, help="Run the test.txt file")
 def runner(day, part, test):
     day = int(day)
     part = int(part)
@@ -46,12 +66,18 @@ def runner(day, part, test):
 
     start = time.time()
     answer = solution(input)
-    print(answer)
-    end = time.time()
 
-    elapsed_seconds = end - start
     print("-------")
+    if answer == -1:
+        print(f"Solution for day {day_string} not yet completed.")
+    else:
+        print(answer)
+    print("-------")
+
+    end = time.time()
+    elapsed_seconds = end - start
     print(f"Completed in {elapsed_seconds} seconds.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     runner()
---
+++ day08/python/solution.py
@@ -0,0 +1,13 @@
+
+def part_one(input):
+    result = -1
+
+    return result
+
+
+def part_two(input):
+    result = -1
+
+    return result
+
+
