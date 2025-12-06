d54b5627edab6e4f3f65e107c63d2e2e35a037d5
NJKode <nathan.kennedy.1997@gmail.com>
2025-12-07 09:48:13 +1300

    Day 6 solution

--- aoc.py
+++ aoc.py
@@ -15,34 +15,6 @@ import day11.python.solution as day11
 import day12.python.solution as day12
 
 
-solutions = {
-    (1, 1): day01.part_one,
-    (1, 2): day01.part_two,
-    (2, 1): day02.part_one,
-    (2, 2): day02.part_two,
-    (3, 1): day03.part_one,
-    (3, 2): day03.part_two,
-    (4, 1): day04.part_one,
-    (4, 2): day04.part_two,
-    (5, 1): day05.part_one,
-    (5, 2): day05.part_two,
-    (6, 1): day06.part_one,
-    (6, 2): day06.part_two,
-    (7, 1): day07.part_one,
-    (7, 2): day07.part_two,
-    (8, 1): day08.part_one,
-    (8, 2): day08.part_two,
-    (9, 1): day09.part_one,
-    (9, 2): day09.part_two,
-    (10, 1): day10.part_one,
-    (10, 2): day10.part_two,
-    (11, 1): day11.part_one,
-    (11, 2): day11.part_two,
-    (12, 1): day12.part_one,
-    (12, 2): day12.part_two,
-}
-
-
 @click.command()
 @click.argument("day")
 @click.argument("part")
@@ -62,7 +34,11 @@ def runner(day, part, test):
     input = text_file.read()
     text_file.close()
 
-    solution = solutions[(day, part)]
+    day_module = globals()[f"day{day_string}"]
+    if part == 1:
+        solution = day_module.part_one
+    elif part == 2:
+        solution = day_module.part_two
 
     start = time.time()
     answer = solution(input)
--- day06/python/solution.py
+++ day06/python/solution.py
@@ -1,13 +1,64 @@
+import math
+
 
 def part_one(input):
-    result = -1
+    lines = input.splitlines()
+
+    grid = [line.split() for line in lines]
+    result = 0
+    for op in range(0, len(grid[0])):
+        operands = []
+        for eq in range(0, len(grid)):
+            operands.append(grid[eq][op])
+
+        operation = operands.pop()
+        operands = [int(op) for op in operands]
+        if operation == "+":
+            result += sum(operands)
+        elif operation == "*":
+            result += math.prod(operands)
 
     return result
 
 
 def part_two(input):
-    result = -1
+    lines = input.splitlines()
+
+    num_rows = len(lines)
+    num_columns = len(lines[0])
+    grid = []
+    for col in range(num_columns - 1, -1, -1):
+        # step backwards from rightmost column
+        vals = []
+        for row in range(0, num_rows):
+            vals.append(lines[row][col])
+        grid.append(vals)
+
+    result = 0
+
+    operands = []
+    for exploded_operand in grid:
+        operator = exploded_operand[-1]
+        if operator == "+" or operator == "*":
+            exploded_operand = exploded_operand[:-1]
+        else:
+            operator = ""
+
+        operand_string = "".join(exploded_operand).strip()
+        if operand_string == "":
+            continue
+
+        operand = int("".join(exploded_operand))
+        operands.append(operand)
+
+        if operator == "":
+            continue
+
+        if operator == "+":
+            result += sum(operands)
+        elif operator == "*":
+            result += math.prod(operands)
+
+        operands = []
 
     return result
-
-
