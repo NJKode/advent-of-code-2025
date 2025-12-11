772c76c1eed00bfab713346d5fb07b87c22df29b
NJKode <nathan.kennedy.1997@gmail.com>
2025-12-11 20:50:29 +1300

    Day 9 solution

--- day09/python/solution.py
+++ day09/python/solution.py
@@ -1,13 +1,62 @@
+import shapely
+from shapely.geometry.polygon import Polygon
+
 
 def part_one(input):
-    result = -1
+    lines = input.splitlines()
 
-    return result
+    coords = []
+    for line in lines:
+        coord = line.split(",")
+        x = int(coord[0])
+        y = int(coord[1])
+        coords.append((x, y))
+
+    max_area = 0
+    for i_a, tile_a in enumerate(coords):
+        for i_b in range(i_a + 1, len(coords)):
+            tile_b = coords[i_b]
+            area = (abs(tile_b[0] - tile_a[0]) + 1) * (abs(tile_b[1] - tile_a[1]) + 1)
+            if area > max_area:
+                max_area = area
+
+    return max_area
 
 
 def part_two(input):
-    result = -1
+    lines = input.splitlines()
 
-    return result
+    coords = []
+    for line in lines:
+        coord = line.split(",")
+        x = int(coord[0])
+        y = int(coord[1])
+        coords.append((x, y))
 
+    max_area = 0
+    polygon = Polygon(coords)
+    shapely.prepare(polygon)
 
+    for i_a, tile_a in enumerate(coords):
+        for i_b in range(i_a + 2, len(coords)):
+            if i_a == i_b:
+                continue
+
+            tile_b = coords[i_b]
+            square_coords = [
+                (tile_a[0], tile_a[1]),
+                (tile_a[0], tile_b[1]),
+                (tile_b[0], tile_b[1]),
+                (tile_b[0], tile_a[1]),
+            ]
+
+            square = Polygon(square_coords)
+
+            if not polygon.covers(square):
+                continue
+
+            area = (abs(tile_b[0] - tile_a[0]) + 1) * (abs(tile_b[1] - tile_a[1]) + 1)
+            if area > max_area:
+                max_area = area
+
+    return max_area
